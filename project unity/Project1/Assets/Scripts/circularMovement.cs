0deb27501d7467de75a2986c2fc752e5ca3f79a0
Student <student@ent-v314-015.local>
2017-12-19 15:57:09 -0500

    updated files

---
+++ project unity/Project1/Assets/Scripts/circularMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class circularMovement : MonoBehaviour {
+
+	// for circular movement we dont really need to 
+	// "keep track" of x and z, we just need to keep track
+	// of "what point of the circle" are we at? like, in degrees,
+	// or in this case in Radians (0 -> 2 pi)
+	private float theta = 0f; 
+	private float pitch = 0f;
+    private float ypos = 0f;
+
+	// and how far from center should it be?
+	public float radius = 5f;
+	
+	void Update () {
+		print(Mathf.Tan(0.5F));
+		// keyboard input changes theta
+		theta += Input.GetAxis ("Horizontal") * .1f;	// multiply by 10%, otherwise it moves too fast
+		pitch += Input.GetAxis ("Vertical") * .1f;	// multiply by 10%, otherwise it moves too fast
+
+		// then derive X and Z location from that, so it move
+		// in a circle
+		float xPos = Mathf.Cos (theta) * radius;
+		float zPos = Mathf.Sin (theta) * radius;// if this were 2d, we would use y instead of z
+        Debug.Log(pitch);
+        Debug.Log(ypos);
+
+		// do it
+		transform.position = new Vector3 (xPos, 1, zPos);
+		transform.position = new Vector3 (xPos,pitch,zPos);
+        
+
+        if ( ypos == 5){
+                pitch = pitch * -1;
+        }
+        if (ypos == -2){
+                pitch = pitch * -1;
+        }
+
+
+    }
+}
---
+++ project unity/Project1/Instructions.md
@@ -0,0 +1,29 @@
+ 
+#Instructions.md
+
+ ##the game that doesnt have a name doesnt have a name
+
+ ## Summary
+ the objective is to defeat the other player
+ player 1 will control an antivirus that revoles around the platform and the player 2 will control the virus that spawn on the board. the objective is to either destroy the other player. Either the virus or anti virus will have to defeat the other.
+ player 1 can win simply by shooting the virus off the platform while to virus player tries to evade and shoot back at the antivirus. If the antivirus take ten hits, the virus player wins.
+ i just wanted to make a simple but competitively fast game. i plan to add a dash button and tweek controls to remain loose but accurate so it can give some kind of excitement while featuring tight controls.
+
+
+ # how to play
+ ##for virus player 
+ directional buttons to move along the platform 
+ a - shoot bullet
+ q - shoot cannon
+ s - dash
+ d - defend
+
+## antivirus player
+mouse - move
+left click - shoot bullet
+right click - shoot cannon
+0 - dash
+. defend
+
+
+ 
